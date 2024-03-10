738b543a86557991760ea54bb374a0d417a5e463
HH Control <hh-ctrl@hh-ctrl.com>
2024-03-10 01:51:46 -0500

    added a file to point to directory

--- file-permissions.js
+++ file-permissions.js
@@ -1,8 +1,18 @@
 const fs =require('fs')
+const path = require('path');
+
+// Read mainDirectory from kipro-target.json
+const scriptDir = path.dirname(__filename);
+const configFile = path.join(scriptDir, 'kipro-target.json');
 
 function fileSort() {
-  const mainDirectory = '/Volumes/1TB HD/Definition Church Dropbox/Creative/RESOURCES/WS_FOOTAGE/'
-
+  try {
+    const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+    mainDirectory = config.mainDirectory;
+  } catch (err) {
+    console.error('Error reading kipro-target.json:', err);
+    process.exit(1);
+  }
   fs.readdir(mainDirectory, (err, files) => {
     if (err)
       console.log(err)
--- kipro-downloader.js
+++ kipro-downloader.js
@@ -1,23 +1,37 @@
-const KiPro = require("./index.js").KiPro
-const fs =require('fs')
-const moveFile = require('move-file')
+const KiPro = require("./index.js").KiPro;
+const fs = require('fs');
+const moveFile = require('move-file');
+const path = require('path');
 
-const mainDirectory = '/Volumes/1TB HD/Definition Church Dropbox/Creative/RESOURCES/WS_FOOTAGE/'
+// Read mainDirectory from kipro-target.json
+const scriptDir = path.dirname(__filename);
+const configFile = path.join(scriptDir, 'kipro-target.json');
 
-const kipro = new KiPro("192.168.15.9")
-
-async function run(){
-  kipro.getClips(function (response) {
-  const dates = response
-  const lastCLips = dates.clips
-  lastCLips.forEach(element => {
-    const dateInfo = new Date(`${element.timestamp.substr(0, 8)}`) 
-    let nameOfClip = dateInfo.toISOString().substr(2, 8) + '_' + element.clipname
-    kipro.getMedia(element.clipname, mainDirectory + nameOfClip, function(file, location) {
-      console.log(file, location)
-      })
-    })
-  })
+let mainDirectory;
+try {
+  const config = JSON.parse(fs.readFileSync(configFile, 'utf8'));
+  mainDirectory = config.mainDirectory;
+} catch (err) {
+  console.error('Error reading kipro-target.json:', err);
+  process.exit(1);
 }
 
-run()
+const kipro = new KiPro("192.168.15.9");
+
+async function run() {
+  kipro.getClips(function(response) {
+    const dates = response;
+    const lastCLips = dates.clips;
+
+    lastCLips.forEach(element => {
+      const dateInfo = new Date(`${element.timestamp.substr(0, 8)}`);
+      let nameOfClip = dateInfo.toISOString().substr(2, 8) + '_' + element.clipname;
+
+      kipro.getMedia(element.clipname, mainDirectory + nameOfClip, function(file, location) {
+        console.log(file, location);
+      });
+    });
+  });
+}
+
+run();
