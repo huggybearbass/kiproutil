0b17ff03fd4c433592a5270e54ab912838bef33d
Ethan Henley <ethan@ethanhenleymusic.com>
2024-08-01 16:47:01 -0400

    fixed date

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ const comingThursday = getComingThursday();
 
 
 module.exports = {
-    mainDirectory: `/mnt/dropbox/Crossroads Church Dropbox/Ethan Henley/24-07-19_Worship-Night/${comingThursday}/2 SERVICE ELEMENTS`,
+    mainDirectory: `/mnt/dropbox/Crossroads Church Dropbox/National/Thursday Service/${comingThursday}/2 SERVICE ELEMENTS`,
     ips: {
       "REC-201": "172.16.11.124",
       "REC-202": "172.16.11.125",
